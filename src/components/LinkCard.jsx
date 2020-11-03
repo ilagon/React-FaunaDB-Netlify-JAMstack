747a3c03e7827b8946a57603975d70fcd81e3f79
Adrian <lorenzyadrian@gmail.com>
2020-11-03 23:40:41 +0800

    Renamed links

--- src/components/LinkCard.jsx
+++ src/components/LinkCard.jsx
@@ -5,7 +5,7 @@ export default function LinkCard({link, refreshLinks}) {
     const archiveLink = async () => {
         link.archived = true;
         try{
-            await fetch('/api/updateLink', {method:'PUT',body: JSON.stringify(link)});
+            await fetch('/.netlify/functions/updateLink', {method:'PUT',body: JSON.stringify(link)});
             refreshLinks();
         } catch(error){
             console.error("BZZZZT", error);
@@ -15,7 +15,7 @@ export default function LinkCard({link, refreshLinks}) {
     const deleteLink = async () => {
         const id = link._id;
         try{
-            await fetch('/api/deleteLink', {method:'DELETE',body: JSON.stringify({id})});
+            await fetch('/.netlify/functions/deleteLink', {method:'DELETE',body: JSON.stringify({id})});
             refreshLinks();
         } catch(error){
             console.error("BZZZZT", error);
